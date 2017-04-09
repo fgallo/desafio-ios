5f2cccb89b5b6d06ccba67e1e655a217a27a729d
Fernando Gallo <fernandgallo@gmail.com>
2017-04-09 13:27:18 -0300

    Segue Identifiers

---
+++ Arena Challenge/Arena Challenge/Helper/SegueIdentifiers.swift
@@ -0,0 +1,13 @@
+//
+//  SegueIdentifiers.swift
+//  Arena Challenge
+//
+//  Created by Fernando Gallo on 09/04/17.
+//  Copyright © 2017 arena. All rights reserved.
+//
+
+import Foundation
+
+enum SegueIdentifiers: String {
+    case RepositoryPullRequestsViewController = "repositoryPullRequestsViewController"
+}
